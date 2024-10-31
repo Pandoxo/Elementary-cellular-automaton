8bcad031d99b092f3dbc1b296a5850d0d81369c8
Hubert <wozniak.hubert.pb+github@gmail.com>
2024-10-31 02:09:15 +0100

    second

--- eca.py
+++ eca.py
@@ -1,36 +1,75 @@
 import pygame as pg
+from copy import deepcopy
 WIDTH, HEIGHT = 1280,1280
 WINDOW = pg.display.set_mode((WIDTH,HEIGHT))
-CELLS_IN_ROW = 256
-CELLS_IN_COLL = CELLS_IN_ROW
-CELL_SIZE = WIDTH/CELLS_IN_ROW
-BOARD = [[False for i in range(CELLS_IN_ROW)] for j in range(CELLS_IN_COLL)]
-BOARD[0][128] = True
+ROWS = 640
+COLLS = ROWS
+CELL_SIZE = WIDTH/COLLS
+BOARD = [[0 for i in range(COLLS)] for j in range(ROWS)]
+BOARD[0][COLLS//2+1] = 1
 
 pg.display.set_caption("Elemetary Celular Automaton")
-
 def draw():
     WINDOW.fill("black")
 
-    for i in range(CELLS_IN_COLL): #Col
-        for j in range(CELLS_IN_ROW): # ROW
-            if BOARD[j][i] == True:
+    for i in range(ROWS): #Col
+        for j in range(COLLS): # ROW
+            if BOARD[i][j] == True:
                 color = "white"
             else:
                 color = "black"
 
-            pg.draw.rect(WINDOW,color,((i*CELL_SIZE,j*CELL_SIZE),(CELL_SIZE,CELL_SIZE)))
+            pg.draw.rect(WINDOW,color,((j*CELL_SIZE,i*CELL_SIZE),(CELL_SIZE,CELL_SIZE)))
     pg.display.update()
 
+def dec_to_bin(num):
+    result = ""
+    while num >0:
+        if num%2==0:
+            result += "0"
+        else:
+            result += "1"
+        num//=2
+    result = result[::-1]
+    if len(result) <8:
+        result = "0" *(8-len(result)) + result
+    return result
 
+    
+def create_rule(rule):
+    rules = {}
+    bin_rule = dec_to_bin(rule)
+    for i in range(8):
+        bin_i = bin(i)[2:]
+        if len(bin_i) <3:
+            bin_i = "0"*(3-len(bin_i)) + bin_i
+        rules[bin_i] = int(bin_rule[i])
+    return rules # return dict ex. {"000":1}
+        
+def next_gen(gen,rules):
+    new_gen = [0 for _ in range(COLLS)]
+
+    for i in range(1,COLLS-2): # we skip first and last index
+        neighbours = "".join([ str(x) for x in gen[i-1:i+2]])#  elements before and after i
+        new_gen[i] = rules[neighbours]
+        
+    return new_gen
+    
 def main():
     running = True
+    rules = create_rule(126)
+    row = 1
     while running:
         for event in pg.event.get():
             if event.type == pg.QUIT:
                 running = False
         draw()
         
+        BOARD[row] = next_gen(BOARD[row-1],rules)
+        row +=1 
+        
+        
+
 
 
 if __name__ == "__main__":
